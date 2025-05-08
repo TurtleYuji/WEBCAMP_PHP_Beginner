b1d7f5ce8041ab5950e0394bd272934f1bf14d6e
Turtle Yuji <ec2-user@ip-172-31-42-173.ap-northeast-1.compute.internal>
2025-05-08 09:36:26 +0000

    operator.phpを書いた

--- define.php
+++ define.php
@@ -7,3 +7,15 @@ define('DEF_STRING', "string");
 var_dump(DEF_INT);
 var_dump(DEF_FLOAT);
 var_dump(DEF_STRING);
+
+$num_string = "999";
+$num_int = 999;
+
+var_dump($num_string);
+var_dump($num_int);
+
+define('TEST_FLOAT', 1.23);
+define('TEST_FLOAT2', "1.23");
+
+var_dump(TEST_FLOAT);
+var_dump(TEST_FLOAT2);
