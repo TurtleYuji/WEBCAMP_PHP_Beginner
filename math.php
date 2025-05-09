5cea2f338995b615d8765cf3af4365317267fa3b
Turtle Yuji <ec2-user@ip-172-31-42-173.ap-northeast-1.compute.internal>
2025-05-09 12:26:26 +0000

    math.phpを書いた

---
+++ math.php
@@ -0,0 +1,18 @@
+<?php
+
+$i = -100;
+$j = abs($i);
+echo "{$i}の絶対値は{$j} \n";
+
+//
+echo "\n";
+
+//
+
+$n =3.5;
+$number_f = floor($n);
+echo "{$number_f}  は 3.5の切り捨て\n";
+
+$number_c = ceil($n);
+echo "{$number_c}  は 3.5の切り上げ\n";
+
