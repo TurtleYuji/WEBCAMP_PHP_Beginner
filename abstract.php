5178de99383e11800baf2a6e80c5fac1f43af06f
Turtle Yuji <ec2-user@ip-172-31-42-173.ap-northeast-1.compute.internal>
2025-05-13 05:32:13 +0000

    trait.phpを書いた

--- abstract.php
+++ abstract.php
@@ -4,8 +4,22 @@ abstract class 抽象クラス
 {
     abstract public function 抽象メソッド();
 
-    public function 普通のメソッド();
+    public function 普通のメソッド()
     {
-        echo ""
+        echo "普通のメソッド \n";
     }
 }
+
+class 子クラス extends 抽象クラス
+{
+    public function 抽象メソッド()
+    {
+        echo "子クラスで実装した抽象のメソッド \n";
+    }
+}
+
+//$obj = new 抽象クラス();
+
+$obj = new 子クラス();
+$obj ->普通のメソッド();
+$obj ->抽象メソッド();
